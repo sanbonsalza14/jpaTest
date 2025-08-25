5eb4c0415a52e821a8058535ac1eb26daeb9a45f
804-03 <slamguy14@naver.com>
2025-08-25 10:50:02 +0900

    문제는문제

--- src/main/java/com/my/jpaTest/repository/UsersRepository.java
+++ src/main/java/com/my/jpaTest/repository/UsersRepository.java
@@ -2,9 +2,9 @@ package com.my.jpaTest.repository;
 
 import com.my.jpaTest.dto.Gender;
 import com.my.jpaTest.entity.Users;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,7 +22,7 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
 
     // 3. 셩별이 여자이고 좋아하는 색상이 Red인 자료
     // select * from users where gender='Female' and like_color='Red';
-    //List<Users> findByGenderAndLikeColor(Gender gender, String color);
+    List<Users> findByGenderAndLikeColor(Gender gender, String color);
 
     // 4. 범위 검색(날짜, 시간)
     // 어제 이후 생성된 모든 자료 검색하기(어제, 오늘...)
@@ -58,7 +58,8 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
 
     // 8.2. email에 org 를 포함하는 데이터(Contains / like)
     // select * from users where email like '%org%';
-//    List<Users> findByEmailContains(String x);
+    List<Users> findByEmailContains(String x);
+
     List<Users> findByEmailLike(String x);
 
     // 9. 정렬
@@ -75,31 +76,38 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
     List<Users> findByLikeColor(String color, Sort sort);
 
     // 문제 1. 여성의 이름 중 "w"또는 "m"을 포함하는 자료를 검색하시오.
-    //select * from users where (gender ='Female" and name like '%w%')
-    //    or(gender= 'Female' and name like '%M%')
-    List<Users> findByGenderAndNameContainingIgnoreCaseOrGenderAndNameContainingIgnoreCase(
-            Gender gender1, String keyword1,
-            Gender gender2, String keyword2
+    List<Users> findByGenderAndNameContainsOrGenderAndNameContains
+    (Gender gender1, String name1, Gender gender2, String name2);
+
+    // 문제 2. 이메일에 net을 포함하는 데이터 건수를 출력
+    // List<Users> findByEmailContains 활용
+
+    // 문제 3. 가장 최근 한달이내에 업데이트된 자료 중 이름 첫자가 "J"인 자료를 출력
+    List<Users> findByUpdatedAtGreaterThanEqualAndNameLike(
+            LocalDateTime start, String data
     );
 
-    // 문제 2. 이메일에 net을 포함하는 데이터 건수를 출력하시오.
-    //select count from users where email like %net%)
-    List<Users> findByEmailContains(String keyword);
-
-    //문제 3. 문제 3. 가장 최근 한달이내에 업데이트된 자료 중 이름 첫자가 "J"인 자료를 출력하시오.
-    //select from users where updatedAt >= :startDate AND name like concat(:refix, '%'))
-    List<Users>findByUpdatedAtGreaterThanEqualAndNameStartingWith(
-            LocalDateTime startDate,
-            String prefix
-    );
-
-    //문제 4. 가장 최근 생성된 자료 10건을 ID, 이름, 성별, 생성일 만 출력하시오.
-    // select id,name,gender,createdAt from users ORDER BY createdAt desc;
+    // 문제 4. 가장 최근 생성된 자료 10건을 ID, 이름, 성별, 생성일 만 출력
     List<Users> findTop10ByOrderByCreatedAtDesc();
 
-    //문제 5. "Red"를 좋아하는 남성 이메일 계정 중 사이트를 제외한 계정만 출력하시오.
-    //(예, apenley2@tripod.com  → apenley2)
-    //select substring(email,1,locate('@', u.email)-1)
-    //from users where gender = :gender and likeColor = :color and locate('@', email) >0
-    List<Users> findByGenderAndLikeColor(Gender gender, String color);
+    //문제 5. "Red"를 좋아하는 남성 이메일 계정 중 사이트를 제외한 계정만 출력
+    //List<Users> findByGenderAndLikeColor(Gender gender, String color);
+
+    //문제 6. 갱신일이 생성일 이전인 잘못된 데이터를 출력하시오.
+
+    //문제 7. 이메일에 edu를 갖는 여성 데이터를 가장 최근 데이터부터 보이도록 출력
+    List<Users> findByGenderAndEmailContainsOrderByCreatedAtDesc(
+            Gender gender, String email
+    );
+
+    //문제 8. 좋아하는 색상 별로 오름차순 정렬하고 같은 색상 데이터는
+    // 이름의 내림차순으로 출력하시오.
+    // Sort() 이용해서 처리
+
+    // 문제 9. 전체 자료를 가장 최근 입력한 자료 순으로 정렬 및 페이징
+    // 처리하고 한 페이지당 10건씩 출력하되,
+    // 그 중 1번째 페이지를 출력하시오.
+
+    // 문제 10. 남성자료 만.
+    List<Users> findByGender(Gender gender, Pageable pageable);
 }
--- src/test/java/com/my/jpaTest/quiz/QuizTest.java
+++ src/test/java/com/my/jpaTest/quiz/QuizTest.java
@@ -9,8 +9,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,10 +26,10 @@ public class QuizTest {
 
     @Test
     @Transactional
-    @DisplayName("Given/when/Then으로 테스트 하기")
+    @DisplayName("Given/When/Then으로 테스트 하기")
     void assertThatTest() {
-        //신규데이터 추가 테스트
-        //Given
+        // 신규데이터 추가 테스트
+        // Given
         Users jin = Users.builder()
                 .name("안유진")
                 .email("jin@korea.com")
@@ -34,63 +38,127 @@ public class QuizTest {
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
                 .build();
-        //When
+        // When
         repository.save(jin);
-        //Then
-        // 이름으로 검색한 결과와  jin 이랑 같으면...성공
+        // Then
+        // 이름으로 검색한 결과와 jin 이랑 같으면...성공
         Users result = repository.findByName("안유진").get(0);
-        //검사
+        // 검사
         Assertions.assertThat(result.getEmail()).isEqualTo(jin.getEmail());
-
-
     }
 
     @Test
-    @DisplayName("문제 1.")
+    @DisplayName("문제 1")
     void findByGenderAndNameContainsOrGenderAndNameContains() {
-        repository.findByGenderAndNameContainingIgnoreCaseOrGenderAndNameContainingIgnoreCase(
-                Gender.Female, "w",
-                Gender.Female, "m"
+        repository
+                .findByGenderAndNameContainsOrGenderAndNameContains(
+                        Gender.Female, "w", Gender.Female, "m")
+                .forEach(x -> System.out.println(x));
+        // 다시 마지막
+    }
+
+    @Test
+    @DisplayName("문제 2")
+    void net을포함하는데이터건수() {
+        System.out.println(
+                repository.findByEmailContains("net")
+                        .stream().count()
+        );
+    }
+
+    @Test
+    @DisplayName("문제 3")
+    void findByUpdatedAtGreaterThanEqualAndNameLike() {
+        LocalDate baseDate = LocalDate.now()
+                .minusMonths(1L)
+                .plusDays(1L);
+        LocalDateTime start = baseDate.atStartOfDay();
+        repository.findByUpdatedAtGreaterThanEqualAndNameLike(
+                start, "J%"
         ).forEach(x -> System.out.println(x));
     }
 
     @Test
-    @DisplayName("문제2: email LIKE %net% 전체 출력")
-    void findByEmailContains() {
-        repository.findByEmailContains("net")
-                .forEach(x -> System.out.println(x));
-    }
-
-    @Test
-    @DisplayName("문제 3. 가장 최근 한달이내에 업데이트된 자료 중 이름 첫자가 \"J\"인 자료를 출력하시오.")
-    void findByUpdatedAtGreaterThanEqualAndNameStartingWith() {
-        LocalDateTime oneMonthAgo = LocalDateTime.now().minusMonths(3);
-        repository.findByUpdatedAtGreaterThanEqualAndNameStartingWith(oneMonthAgo, "J")
-                .forEach(x -> System.out.println(x));
-    }
-
-    @Test
-    @DisplayName("문제 4. 가장 최근 생성된 자료 10건을 ID, 이름, 성별, 생성일 만 출력하시오.")
+    @DisplayName("문제 4")
+        // ID, 이름, 성별, 생성일
     void findTop10ByOrderByCreatedAtDesc() {
-        repository.findTop10ByOrderByCreatedAtDesc()
-                .forEach(x-> System.out.println(
-                        "ID:" + x.getId()+
-                        ",이름:" + x.getName() +
-                        ",성별:" + x.getGender() +
-                        ", 생성일 :" + x.getCreatedAt()
-                ));
+        List<Users> results = repository.findTop10ByOrderByCreatedAtDesc();
+        for (Users user : results) {
+            System.out.println("ID : " + user.getId() +
+                    ", Name : " + user.getName() +
+                    ", Gender : " + user.getGender() +
+                    ", CreatedAt : " + user.getCreatedAt());
+        }
     }
 
     @Test
-    @DisplayName("문제 5. \"Red\"를 좋아하는 남성 이메일 계정 중 사이트를 제외한 계정만 출력하시오.\n" +
-            "(예, apenley2@tripod.com  → apenley2)")
+    @DisplayName("문제 5")
+        // 사이트를 제외한 계정만 출력
     void findByGenderAndLikeColor() {
-        List<Users>maleRed = repository.findByGenderAndLikeColor(Gender.Male, "Red");
-        maleRed.stream().map(user -> user.getEmail().split("@")[0]) //@기준으로 앞부분만 추출
-                .forEach(x -> System.out.println(x));
-
+        List<Users> results = repository.findByGenderAndLikeColor(
+                Gender.Male, "Red"
+        );
+        for (Users user : results) {
+            String mail = user.getEmail();
+            String account = mail.substring(0, mail.indexOf("@"));
+            System.out.println("Email : " + mail + ",Account : " + account);
+        }
     }
 
+    //문제 6. 갱신일이 생성일 이전인 잘못된 데이터를 출력하시오.
+    @Test
+    @DisplayName("문제 6")
+    void errorDataList() {
+        List<Users> users = repository.findAll();
+        for (Users user : users) {
+            if (user.getCreatedAt().isAfter(user.getUpdatedAt())) {
+                System.out.println(user);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("문제 7")
+    void findByGenderAndEmailContainsOrderByCreatedAtDesc() {
+        repository
+                .findByGenderAndEmailContainsOrderByCreatedAtDesc(
+                        Gender.Female, "edu"
+                )
+                .forEach(x -> System.out.println(x));
+    }
+
+    @Test
+    @DisplayName("문제 8")
+    void likeColorSort() {
+        List<Users> result = repository.findAll(
+                Sort.by(Sort.Order.asc("likeColor"),
+                        Sort.Order.desc("name"))
+        );
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(result.get(i));
+        }
+    }
+
+    @Test
+    @DisplayName("문제 9")
+    void sortAndPaging() {
+        Sort sort = Sort.by(Sort.Order.desc("updatedAt"));
+        Pageable pageable = PageRequest
+                .of(0, 10, sort);
+        repository.findAll(pageable).getContent()
+                .forEach(x -> System.out.println(x));
+    }
+
+    // 문제10. 남성 자료를 ID의 내림차순으로 정렬한 후 한페이당 3건을 출력하되
+    // 그 중 2번째 페이지 자료를  출력하시오.
+    @Test
+    @DisplayName("문제 10")
+    void manDataPaging() {
+        Sort sort = Sort.by(Sort.Order.desc("id"));
+        Pageable pageable = PageRequest.of(1, 3, sort);
+        repository.findByGender(Gender.Male, pageable)
+                .forEach(x-> System.out.println(x));
+    }
 }
 
 
