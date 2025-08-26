a2e8b894597129ca366db3dabe78d2537ffc8df0
804-03 <slamguy14@naver.com>
2025-08-26 12:03:36 +0900

    양방향연관관계설정

--- src/main/java/com/my/jpaTest/service/ContextService.java
+++ src/main/java/com/my/jpaTest/service/ContextService.java
@@ -2,44 +2,29 @@ package com.my.jpaTest.service;
 
 import com.my.jpaTest.entity.Member;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class ContextService {
     @Autowired
     EntityManager em;
 
-    //정장하는 작업을 수행
+    // 저장하는 작업을 수행
     public Member memberInsert() {
         Member jang = Member.builder()
                 .memberId("jang")
                 .name("장원영")
                 .build();
-        //영속성 공간에 저장하는 명령
+        // 영속성 공간에 저장하는 명령
         em.persist(jang);
 
         // member_id : jang 인 레코드를 찾아서 반환
         // Member 테이블에서 키 값이 jang 인 친구를 찾는 거.
         Member won = em.find(Member.class, "jang");
         return won;
-    }
-
-    public void transactionTest() {
-        // 지연도니 쓰기 테스트
-        Member ahn = Member.builder()
-                .memberId("jin")
-                .name("안유진")
-                .build();
-
-        Member carina = Member.builder()
-                .memberId("carina")
-                .name("카리나")
-                .build();
-
-        em.persist(ahn);
-        em.persist(carina);
-        em.flush();
     }
 
     public void dirtyCheckingTest() {
