78d0e9346872121dd8d3e36ae1dadc74bf64504e
804-03 <slamguy14@naver.com>
2025-08-28 09:53:39 +0900

    1. Test 생성 2. Getter,Setter 만듬

---
+++ src/main/java/com/my/jpaTest/dto/Test.java
@@ -0,0 +1,12 @@
+package com.my.jpaTest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+public class Test {
+    private Long Id;
+    private String name;
+}
